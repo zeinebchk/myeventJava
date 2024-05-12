9b328db0a22140393c621874f1cefb0eb79e7899
ahmedbenabdelaziz <ahmedaba1652001@gmail.com>
2024-05-12 11:54:28 +0100

    Votre message de commit ici

---
+++ src/main/java/com/example/myevent/ChefProjet/LoginController.java
@@ -0,0 +1,85 @@
+package com.example.myevent.ChefProjet;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginController {
+
+    @FXML
+    private Button nextPageButton; // Assurez-vous que l'identifiant fx:id correspond à celui dans votre fichier FXML
+    @FXML
+    private Button nextPageButtonn; // Assurez-vous que l'identifiant fx:id correspond à celui dans votre fichier FXML
+    @FXML
+    private Button studio_btn;
+    @FXML
+    private Button venue_btn;
+
+    @FXML
+    private void handleConnexionButtonAction(ActionEvent event) {
+        // Charger la nouvelle page
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) nextPageButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+
+            // Imprimer un message
+            System.out.println("Navigué vers la deuxième page avec succès !");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handleConnexionButtonActionn(ActionEvent event) {
+        // Charger la nouvelle page
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/signup.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) nextPageButtonn.getScene().getWindow();
+            stage.setScene(new Scene(root));
+
+            // Imprimer un message
+            System.out.println("Navigué vers la deuxième page avec succès !");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handleConnexionButtonActionnListeClient(ActionEvent event) {
+        // Charger la nouvelle page
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/hello-view.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) studio_btn.getScene().getWindow();
+            stage.setScene(new Scene(root));
+
+            // Imprimer un message
+            System.out.println("Navigué vers la deuxième page avec succès !");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handleConnexionButtonActionnListeTransaction(ActionEvent event) {
+        // Charger la nouvelle page
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Transaction.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) venue_btn.getScene().getWindow();
+            stage.setScene(new Scene(root));
+
+            // Imprimer un message
+            System.out.println("Navigué vers la deuxième page avec succès !");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,13 @@
 module com.example.myevent {
+    opens com.example.myevent.ChefProjet to javafx.fxml, javafx.base;
+
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
     requires com.dlsc.formsfx;
-    requires mysql.connector.j;
-    requires AnimateFX;
-    requires jBCrypt;
+    requires mysql.connector.java;
     requires java.desktop;
+    requires AnimateFX;
     exports com.example.myevent.controllers to javafx.fxml;
     opens com.example.myevent.controllers to javafx.fxml;
     opens com.example.myevent to javafx.fxml;
